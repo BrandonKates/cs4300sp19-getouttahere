dbc297a88c630fea0b653b5a54223b697a6487a6
David <dpm247@cornell.edu>
2019-04-10 18:39:31 -0400

    advanced search linked to main search

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -8,11 +8,15 @@ net_id = "ams698, bjk224, dpm247, ne236, sn529"
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
+	price = request.args.get('price')
+	group = request.args.get('group')
+	climate = request.args.get('climate')
+	
 	if not query:
 		data = []
 		output_message = ''
 	else:
-		output_message = "Your search: " + query
+		output_message = "Your search: " + " "  + query + " " + price + " " + group + " " + climate
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
