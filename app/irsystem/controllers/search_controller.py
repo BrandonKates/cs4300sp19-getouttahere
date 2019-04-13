776aab1aec2f7dbc63deea8d4b06b60e0095cc3b
Brandon Kates <bjk224@cornell.edu>
2019-04-13 11:13:00 -0400

    Docker updates + requirements

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -8,6 +8,8 @@ net_id = "ams698, bjk224, dpm247, ne236, sn529"
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
+	advanced_query = request.args.get('price')
+	print(advanced_query)
 	if not query:
 		data = []
 		output_message = ''
@@ -15,6 +17,4 @@ def search():
 		output_message = "Your search: " + query
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
 
